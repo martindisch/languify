34005b1bfedd0eed988a974bab298a5ce9c7dca9
Martin Disch <martindisch@gmail.com>
2021-08-21 12:46:55 +0200

    Add permissive CORS to api

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,8 +1,10 @@
+use actix_cors::Cors;
 use actix_web::{post, web, App, HttpResponse, HttpServer, Responder};
 use serde::{Deserialize, Serialize};
 
 #[post("/api/v1/texts/unclassified/_next")]
 async fn get_unclassified() -> impl Responder {
+    println!("Serving response");
     HttpResponse::Ok().json(TextResponse {
         id: 1,
         text: "The quick brown fox jumps over the lazy dog",
@@ -23,7 +25,10 @@ async fn add_classified(
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
-        App::new().service(get_unclassified).service(add_classified)
+        App::new()
+            .wrap(Cors::permissive())
+            .service(get_unclassified)
+            .service(add_classified)
     })
     .bind(("0.0.0.0", 8080))?
     .run()
