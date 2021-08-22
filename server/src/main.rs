494894ff6fef9f5b912e4044ce2b7acdd2aa27a5
Martin Disch <martindisch@gmail.com>
2021-08-22 13:43:48 +0200

    Return next text & remove when classified

--- server/src/main.rs
+++ server/src/main.rs
@@ -5,25 +5,38 @@ use actix_web::{
 use log::info;
 use log::LevelFilter;
 use serde::{Deserialize, Serialize};
+use std::{collections::HashMap, sync::Mutex};
+
+use persistence::UnclassifiedText;
 
 mod persistence;
 
 #[post("/api/v1/texts/unclassified/_next")]
-async fn get_unclassified() -> impl Responder {
+async fn get_unclassified(
+    unclassified_texts: web::Data<Mutex<HashMap<usize, UnclassifiedText>>>,
+) -> impl Responder {
+    let unclassified_texts = unclassified_texts.lock().unwrap();
+    let (&id, unclassified_text) = unclassified_texts.iter().next().unwrap();
+
     HttpResponse::Ok().json(TextResponse {
-        id: 1,
-        text: "The quick brown fox jumps over the lazy dog",
+        id,
+        text: &unclassified_text.text,
     })
 }
 
 #[post("/api/v1/texts/classified")]
 async fn add_classified(
     classification: web::Json<ClassificationRequest>,
+    unclassified_texts: web::Data<Mutex<HashMap<usize, UnclassifiedText>>>,
 ) -> impl Responder {
     info!(
         "Got classification for text {} as {}",
         classification.id, classification.language
     );
+
+    let mut unclassified_texts = unclassified_texts.lock().unwrap();
+    unclassified_texts.remove_entry(&classification.id);
+
     HttpResponse::NoContent()
 }
 
@@ -32,13 +45,15 @@ async fn main() -> std::io::Result<()> {
     env_logger::builder().filter_level(LevelFilter::Info).init();
 
     info!("Loading unclassified texts from CSV");
-    let unclassified_texts =
-        persistence::load_unclassified("unclassified.csv");
+    let unclassified_texts = web::Data::new(Mutex::new(
+        persistence::load_unclassified("unclassified.csv").unwrap(),
+    ));
 
-    HttpServer::new(|| {
+    HttpServer::new(move || {
         let logger = Logger::default();
 
         App::new()
+            .app_data(unclassified_texts.clone())
             .wrap(logger)
             .wrap(Cors::permissive())
             .service(get_unclassified)
