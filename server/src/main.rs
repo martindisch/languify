7b2f3b1b64c9d55f9b2daae470bf5d2449a4f3a1
Martin Disch <martindisch@gmail.com>
2021-08-22 18:26:02 +0200

    Store classifications in file

--- server/src/main.rs
+++ server/src/main.rs
@@ -5,12 +5,20 @@ use actix_web::{
 use log::info;
 use log::LevelFilter;
 use serde::{Deserialize, Serialize};
-use std::{collections::hash_map, sync::Mutex};
+use std::{
+    collections::hash_map,
+    sync::{
+        mpsc::{self, Sender},
+        Mutex,
+    },
+    thread,
+};
 
 use persistence::UnclassifiedText;
 
 mod persistence;
 
+// TODO: move handlers into module
 #[post("/api/v1/texts/unclassified/_next")]
 async fn get_unclassified(
     unclassified_texts: web::Data<
@@ -33,11 +41,14 @@ async fn get_unclassified(
 #[post("/api/v1/texts/classified")]
 async fn add_classified(
     classification: web::Json<ClassificationRequest>,
+    tx: web::Data<Sender<ClassificationRequest>>,
 ) -> impl Responder {
     info!(
         "Got classification for text {} as {}",
         classification.id, classification.language
     );
+
+    tx.send(classification.into_inner()).unwrap();
 
     HttpResponse::NoContent()
 }
@@ -47,17 +58,31 @@ async fn main() -> std::io::Result<()> {
     env_logger::builder().filter_level(LevelFilter::Info).init();
 
     info!("Loading unclassified texts from CSV");
-    let unclassified_texts = web::Data::new(Mutex::new(
+    let (headers, unclassified_texts) =
         persistence::load_unclassified("unclassified.csv")
-            .expect("Unable to load unclassified texts from CSV")
-            .into_iter(),
-    ));
+            .expect("Unable to load unclassified texts from CSV");
+
+    info!("Spawning classified texts writer");
+    let (tx, rx) = mpsc::channel();
+    let unclassified_texts_copy = unclassified_texts.clone();
+    thread::spawn(|| {
+        persistence::classified_writer(
+            "classified.csv",
+            headers,
+            unclassified_texts_copy,
+            rx,
+        )
+    });
+
+    let unclassified_texts =
+        web::Data::new(Mutex::new(unclassified_texts.into_iter()));
 
     HttpServer::new(move || {
         let logger = Logger::default();
 
         App::new()
             .app_data(unclassified_texts.clone())
+            .data(tx.clone())
             .wrap(logger)
             .wrap(Cors::permissive())
             .service(get_unclassified)
@@ -75,7 +100,7 @@ struct TextResponse<'a> {
 }
 
 #[derive(Debug, Deserialize)]
-struct ClassificationRequest {
+pub struct ClassificationRequest {
     id: usize,
     language: String,
 }
--- server/src/persistence.rs
+++ server/src/persistence.rs
@@ -1,34 +1,80 @@
+use csv::Writer;
 use eyre::Result;
-use std::{collections::HashMap, path::Path};
+use std::{
+    collections::HashMap, fs::OpenOptions, path::Path, sync::mpsc::Receiver,
+};
+
+// TODO: put the type somewhere more appropriate
+use super::ClassificationRequest;
 
 pub fn load_unclassified(
     path: impl AsRef<Path>,
-) -> Result<HashMap<usize, UnclassifiedText>> {
+) -> Result<(Vec<String>, HashMap<usize, UnclassifiedText>)> {
     let mut unclassified_texts = HashMap::new();
     let mut reader = csv::Reader::from_path(path)?;
 
-    for (i, result) in reader.records().enumerate() {
-        let record = result?;
-        let detected_languages = record
+    let headers = reader.headers()?.iter().map(str::to_owned).collect();
+
+    for (i, record) in reader.records().map(Result::unwrap).enumerate() {
+        let record: Vec<_> = record.into_iter().collect();
+
+        let detected_languages: Vec<String> = record[0..record.len() - 2]
             .iter()
-            .take(record.len() - 2)
-            .map(str::to_owned)
+            .map(|s| s.to_string())
             .collect();
-        let text = record.iter().last().unwrap().to_owned();
+        let id = record[record.len() - 2].to_string();
+        let text = record[record.len() - 1].to_string();
 
         let unclassified_text = UnclassifiedText {
             detected_languages,
+            id,
             text,
         };
 
         unclassified_texts.insert(i, unclassified_text);
     }
 
-    Ok(unclassified_texts)
+    Ok((headers, unclassified_texts))
 }
 
-#[derive(Debug)]
+pub fn classified_writer(
+    output: impl AsRef<Path>,
+    mut headers: Vec<String>,
+    mut unclassified_texts: HashMap<usize, UnclassifiedText>,
+    rx: Receiver<ClassificationRequest>,
+) -> Result<()> {
+    let file_existed = output.as_ref().exists();
+
+    let file = OpenOptions::new().append(true).create(true).open(output)?;
+    let mut writer = Writer::from_writer(file);
+
+    if !file_existed {
+        headers.push("human".into());
+        writer.write_record(headers)?;
+        writer.flush()?;
+    }
+
+    for classified_text in rx {
+        if let Some(unclassified_text) =
+            unclassified_texts.remove(&classified_text.id)
+        {
+            let mut record = unclassified_text.detected_languages;
+            record.push(unclassified_text.id);
+            record.push(unclassified_text.text);
+            record.push(classified_text.language);
+
+            writer.write_record(record)?;
+            writer.flush()?;
+        }
+    }
+
+    Ok(())
+}
+
+// TODO: move more logic into impls on this type
+#[derive(Debug, Clone)]
 pub struct UnclassifiedText {
     pub detected_languages: Vec<String>,
+    pub id: String,
     pub text: String,
 }
