57f26a034f48e4cf47ae96148ff09e90f6285986
Martin Disch <martindisch@gmail.com>
2021-08-23 20:22:27 +0200

    Have server listen internally
    Will be exposed through reverse proxy.

--- server/src/main.rs
+++ server/src/main.rs
@@ -50,7 +50,7 @@ async fn main() -> std::io::Result<()> {
             .service(handlers::get_unclassified)
             .service(handlers::add_classified)
     })
-    .bind(("0.0.0.0", 8080))?
+    .bind(("127.0.0.1", 8080))?
     .run()
     .await
 }
